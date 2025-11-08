30ee1b6aed5561b71bcf05d0c4137120fd086192
Szymon <stszymek@gmail.com>
2025-11-08 20:08:43 +0100

    works with db localy and dockerized

--- app/db/database.py
+++ app/db/database.py
@@ -6,8 +6,12 @@ from app.env import user, password, db_name
 
 DB_USER = os.getenv("DB_USER", user)
 DB_PASSWORD = os.getenv("DB_PASSWORD", password)
-DB_HOST = os.getenv("DB_HOST", "postgres")
-DB_PORT = os.getenv("DB_PORT", "5432")
+if os.getenv("DOCKER_ENV", "0") == "1":
+    DB_HOST = "postgres"
+    DB_PORT = os.getenv("DB_PORT", "5432")
+else:
+    DB_HOST = "localhost"
+    DB_PORT = os.getenv("DB_PORT", "15432")
 DB_NAME = os.getenv("DB_NAME",  db_name)
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
