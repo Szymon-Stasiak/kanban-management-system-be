9d5401fad2e7035735562e2ea3fe6bd9409bc487
Szymon <stszymek@gmail.com>
2025-12-03 10:55:37 +0100

    FIX FOR DB FOR KAROL

--- app/db/database.py
+++ app/db/database.py
@@ -6,14 +6,21 @@ from app.env import user, password, db_name
 
 DB_USER = os.getenv("DB_USER", user)
 DB_PASSWORD = os.getenv("DB_PASSWORD", password)
+# if os.getenv("DOCKER_ENV", "0") == "1":
+#     DB_HOST = "postgres"
+#     DB_PORT = os.getenv("DB_PORT", "5432")
+# else:
+#     DB_HOST = "localhost"
+#     DB_PORT = os.getenv("DB_PORT", "15432")
 # # if os.getenv("DOCKER_ENV", "0") == "1":
 # DB_HOST = "postgres"
 # DB_PORT = os.getenv("DB_PORT", "5432")
 # else:
-# Prefer environment variables so the app works both locally and in Docker.
-# When running with Docker Compose the DB service name will be `postgres`.
-DB_HOST = os.getenv("DB_HOST", "postgres")
-DB_PORT = os.getenv("DB_PORT", "5432")
+DB_HOST = "localhost"
+DB_PORT = os.getenv("DB_PORT", "15432")
+    # FOR KAROL
+    # DB_HOST = os.getenv("DB_HOST", "postgres")
+    # DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME",  db_name)
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
@@ -36,3 +43,4 @@ if __name__ == "__main__":
             print("Database connection successful.")
     except Exception as e:
         print(f"Database connection failed: {e}")
+
